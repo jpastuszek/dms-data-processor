11665730a3a1b2fe10840dc783adf80d687063b3
Jakub Pastuszek <jpastuszek@gmail.com>
2012-03-01 18:16:46 +0100

    moved out cout data processor builder to separate file

---
+++ spec/data_processor_builder_test2.rb
@@ -0,0 +1,18 @@
+DataProcessorBuilder.new('system CPU count', 'count') do
+	tag 'hello'
+	tag 'world'
+
+	classifier('count').select do
+		key 'system/CPU usage/CPU[user]'
+	end.group do |raw_data_key|
+		by raw_data_key.location
+	end.need do
+		key 'system/CPU usage/CPU[user]'
+	end.each_group do |group, raw_data_keys|
+		tag "location:#{group.first}"
+		tag "system:CPU count"
+	end.process_with do |time_from, time_to, data_sources|
+		collect 'count', time_from, data_sources.length
+	end
+end
+
